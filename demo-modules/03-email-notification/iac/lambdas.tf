5975a1dff1fccdd10b879f46021b27c7412e8710
Andre <andrevitorlopes@gmail.com>
2025-01-12 17:50:26 +0100

    Test create lambda

--- demo-modules/03-email-notification/iac/lambdas.tf
+++ demo-modules/03-email-notification/iac/lambdas.tf
@@ -2,6 +2,8 @@ module "get_movie_lambda" {
   source   = "./modules/lambda"
   name     = "get-movie"
   policies = [data.aws_iam_policy_document.get_movie_item.json]
+  environment_variables = {
+  }
 }
 
 module "create_movie_lambda" {
@@ -13,6 +15,7 @@ module "create_movie_lambda" {
   ]
 
   environment_variables = {
+    TABLE_NAME    = aws_dynamodb_table.movies.name
     SNS_TOPIC_ARN = "arn:aws:sns:${var.region}:${var.account_id}:${local.movies_update_topic_name}"
   }
 }
